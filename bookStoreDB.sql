a549ed2d4548055f34fa09be9462d9b1e0e7df6a
Jesifar M <jesifar27@gmail.com>
2026-07-29 12:43:44 +0530

    Added SQL script for authors and books tables with modifications

---
+++ bookStoreDB.sql
@@ -0,0 +1,20 @@
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    country VARCHAR(100)
+);
+
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(100),
+    price DECIMAL(10,2),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+ALTER TABLE books
+ADD published_year INT;
+
+TRUNCATE TABLE books;
+
+DROP DATABASE BookStoreDB;
