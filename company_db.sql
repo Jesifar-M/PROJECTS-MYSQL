a0b3d5f8e239d8cf7cedba4de368cf7641c26f00
Jesifar M <jesifar27@gmail.com>
2026-07-30 16:52:43 +0530

    Added SQL script for company database with employees and departments tables

---
+++ company_db.sql
@@ -0,0 +1,43 @@
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE departments (
+    emp_id INT,
+    department_name VARCHAR(50)
+);
+
+INSERT INTO employees VALUES
+(1, 'Anjali'),
+(2, 'Rohan'),
+(3, 'Meena');
+
+INSERT INTO departments VALUES
+(1, 'HR'),
+(2, 'IT'),
+(4, 'Finance');
+
+SELECT employees.id,
+       employees.name,
+       departments.department_name
+FROM employees
+LEFT JOIN departments
+ON employees.id = departments.emp_id;
+
+
+SELECT employees.id,
+       employees.name,
+       departments.department_name
+FROM employees
+INNER JOIN departments
+ON employees.id = departments.emp_id;
+
+
+SELECT employees.id,
+       employees.name,
+       departments.department_name
+FROM employees
+RIGHT JOIN departments
+ON employees.id = departments.emp_id;
+
